5c41c4bef9d200686deca1473c1b9a1b6e33545e
fairlytalk <fairlytalk7@gmail.com>
2023-07-13 19:07:53 +0530

    hello there
    my first blog website

---
+++ assets/js/script.js
@@ -0,0 +1,103 @@
+'use strict';
+
+// navbar variables
+const nav = document.querySelector('.mobile-nav');
+const navMenuBtn = document.querySelector('.nav-menu-btn');
+const navCloseBtn = document.querySelector('.nav-close-btn');
+
+
+// navToggle function
+const navToggleFunc = function () { nav.classList.toggle('active'); }
+
+navMenuBtn.addEventListener('click', navToggleFunc);
+navCloseBtn.addEventListener('click', navToggleFunc);
+
+
+
+// theme toggle variables
+const themeBtn = document.querySelectorAll('.theme-btn');
+
+
+for (let i = 0; i < themeBtn.length; i++) {
+
+  themeBtn[i].addEventListener('click', function () {
+
+    // toggle `light-theme` & `dark-theme` class from `body`
+    // when clicked `theme-btn`
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    for (let i = 0; i < themeBtn.length; i++) {
+
+      // When the `theme-btn` is clicked,
+      // it toggles classes between `light` & `dark` for all `theme-btn`.
+      themeBtn[i].classList.toggle('light');
+      themeBtn[i].classList.toggle('dark');
+
+    }
+
+  })
+
+}
+ 
+
+  // close navbar when click on any navbar links
+ 
+
+// const navLinks = document.querySelectorAll("[data-nav-link]");
+
+// const closeNavbar = function () {
+//   navbar.classList.remove("active");
+//   overlay.classList.remove("active");
+// }
+
+// addEventOnElem(navLinks, "click", closeNavbar);
+
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(question => {
+  question.addEventListener('click', () => {
+    const answer = question.querySelector('.answer');
+
+    if (answer.style.display === 'block') {
+      answer.style.display = 'none';
+    } else {
+      answer.style.display = 'block';
+    }
+  });
+});
+
+
+
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
